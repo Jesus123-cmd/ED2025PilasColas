8d1422902b9bbdff0d7bb9bf284a3627cdcbbef3
Jesus Ballesteros <ballesterosjesus900@gmail.com>
2025-02-25 21:47:53 -0700

    agregacion de codigos al apartado de listas

--- LISTASS/COLAS.cs
+++ LISTASS/COLAS.cs
@@ -4,7 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Arreglosusing System;
+namespace Arreglos
+{
+    internal class COLAS
+    {
+    }
+}
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
