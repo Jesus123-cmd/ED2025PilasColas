a8a2cf37c00dc783a95acb62e003193a2c04b0ae
Jesus Ballesteros <ballesterosjesus900@gmail.com>
2025-02-25 21:52:47 -0700

    agregacion de codigos al area de listas

---
+++ Arreglos/PILAS.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglosusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PERSONAL_PRJECT
+
+
+{
+    internal class Colas
+    {
+        private List<string> lista;
+        public Colas()
+        {
+            lista = new List<string>();
+        }
+        public void Agregar(string valor)
+        {
+            lista.Add(valor);
+        }
+        private bool EstaVacia()
+        {
+            return lista.Count == 0;
+        }
+        public void Eliminar()
+        {
+            if (EstaVacia())
+            {
+                throw new Exception("La cola está vacía");
+            }
+            lista.RemoveAt(0);
+        }
+        public string Imprimir()
+        {
+            string datos = string.Empty;
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                string coma = (i == lista.Count - 1) ? "" : ",";
+                datos += lista[i] + coma;
+            }
+            return datos;
+        }
+    }
+}
